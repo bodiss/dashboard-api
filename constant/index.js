18d2baeecdc4b61565a8c4fff99383a9390ba0ee
Bogdan Serhienko <bodis90@gmail.com>
2020-11-16 21:00:32 +0200

    Small refactor; Add new fields to device model;

---
+++ constant/index.js
@@ -0,0 +1,22 @@
+const randWords = [
+  'name',
+  'sensor',
+  'event',
+  'status',
+  'panic',
+  'metric',
+  'type',
+  'model_name',
+  'model_ID',
+  'bodyColor',
+  'firmwareVersion',
+  'lastSyncTime',
+  'alias',
+  'deviceStatus',
+  'totalJobs',
+  'printTimeComplete',
+];
+
+module.exports = {
+  randWords,
+};
